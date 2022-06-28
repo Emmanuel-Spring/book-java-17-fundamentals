8d4a0ccd0ac6c372b8134b7fe6295e1af2b267c8
Emmanuel-Spring <e.nieto@alumnos.santotomas.cl>
2022-06-28 18:22:12 -0400

    primer commit del libro

---
+++ src/test/java/cl/springs/book/BookJavaCodingProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.springs.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookJavaCodingProblemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
