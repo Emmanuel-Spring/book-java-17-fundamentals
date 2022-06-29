0756e44e5a1dca41eb806ed770fa2537d90c2846
Emmanuel Nieto <emmanuel.nieto@tisal.cl>
2022-06-28 21:56:30 -0400

    primer commit del libro

---
+++ src/main/java/cl/springs/book/BookJava17FundamentalsApplication.java
@@ -0,0 +1,13 @@
+package cl.springs.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookJava17FundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookJava17FundamentalsApplication.class, args);
+	}
+
+}
